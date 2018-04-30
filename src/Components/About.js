6bf8176b9b0f7d1ace11e5053777387d1b31eadc
JavaScriptErika <erikajritter@gmail.com>
2018-04-30 15:58:45 -0400

    add cascading coders info/style to <About />

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ccLogo from './../images/cc-logo.svg';
 
 class About extends Component {
   componentDidMount() {
@@ -75,6 +76,53 @@ class About extends Component {
 ]`}
             </code>
           </pre>
+        </div>
+        <div className="about-cascading-coders">
+          <div className="cc-logo-wrap">
+            <img src={ccLogo} alt="Cascading Coders meatball logo" />
+          </div>
+          <div>
+            <h3>
+              In March 2018, I founded a group called Cascading Coders. We're a welcoming community of developers of all
+              skill levels who have a love or interest in creative code.
+            </h3>
+            <h3>
+              We curate content/resources, hold CSS/SVG/Canvas creative code challenges voted on by members, and inspire
+              each other to become better Cascading Coders.
+            </h3>
+            <div class="join-cc-wrap">
+              <div className="join-cc-info">
+                <h3>👥 Join Us</h3>
+                <ul>
+                  <li>
+                    <a href="https://discordapp.com/invite/5P3kYbP" target="_blank">
+                      Discord
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.facebook.com/groups/1752972351391061/" target="_blank">
+                      FB Group
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="join-cc-info">
+                <h3>💜 & Follow</h3>
+                <ul>
+                  <li>
+                    <a href="https://twitter.com/cascadingcoders" target="_blank">
+                      Twitter
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.facebook.com/CascadingCoders/" target="_blank">
+                      FB Page
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
